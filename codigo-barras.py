975c5a5f1d81ab430b35303624ce9716c1e8a79b
ElectronicMakerSpace <temperaturasensors@gmail.com>
2019-11-08 12:46:51 -0600

    2dos intenti github

--- codigo-barras.py
+++ codigo-barras.py
@@ -1,11 +1,11 @@
-import os, socket
+import os, socket, time
 contador = 0
 numero = 0
 
 while(contador==0):
-    numero = int(input("Leyendo Etiquetas:"))
-    while(numero != 0):
+    numero = int(input("Lector de etiquetas:"))
+    while(numero <= 15):
         print ("Etiqueta Leida: {0}\n".format(numero))
-        numero = 0
+        numero = numero + 1 
         #contador=contador+1
         #comentario de pruebas unitarias
